8a835083ce3e70c673b65928421ab8c94dbd79a1
dingyq <dingyq23@qq.com>
2015-07-23 20:58:50 +0800

    Initial Commit

---
+++ BeatPlaneTests/BeatPlaneTests.m
@@ -0,0 +1,40 @@
+//
+//  BeatPlaneTests.m
+//  BeatPlaneTests
+//
+//  Created by bigqiang on 15/7/23.
+//  Copyright (c) 2015年 barbecue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BeatPlaneTests : XCTestCase
+
+@end
+
+@implementation BeatPlaneTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
